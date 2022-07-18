74486592bc7cb17ead81193771f2dd6e74262da1
김익현 <kimikhyun@ikkim01-MacBookAir.local>
2022-07-18 17:44:43 +0900

    ItemDetail

--- src/pages/ItemDetail/ItemDetail.js
+++ src/pages/ItemDetail/ItemDetail.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './ItemDetail.scss';
 
 const ItemDetail = () => {
-  return <div />;
+  return (
+    <div className="itemdetail">
+      <div className="item-img-ul">
+        <div className="item-img-box" />
+        <div className="item-ul" />
+      </div>
+    </div>
+  );
 };
 
 export default ItemDetail;
