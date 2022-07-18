972d028433c08c9a01872a8992afdfc2e87fdefc
김익현 <kimikhyun@ikkim01-MacBookAir.local>
2022-07-18 16:21:21 +0900

    First Setting

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,7 @@
+import './Nav.scss';
+
+function Nav() {
+  return <div>Hello</div>;
+}
+
+export default Nav;
---
+++ src/Router.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from '../src/Components/Nav/Nav';
+
+import Cart from '../src/pages/Cart/Cart';
+import ItemDetail from '../src/pages/ItemDetail/ItemDetail';
+import ItemList from '../src/pages/ItemList/ItemList';
+import Login from '../src/pages/Login/Login';
+import Main from '../src/pages/Main/Main';
+import Register from '../src/pages/Register/Register';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/itemdetail" element={<ItemDetail />} />
+        <Route path="/itemlist" element={<ItemList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
