abbce8e4aa60bab7360b318f7460ff4be3ae28f9
이금관 <igeumgwan@eric.local>
2022-07-19 11:52:10 +0900

    add : 레이아웃

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -12,6 +12,58 @@ const Register = () => {
           </button>
         </div>
       </section>
+      <section className="register-body">
+        <div className="register-container">
+          <div className="header-box">
+            <div className="header">기본정보 (필수)</div>
+            <div className="header-content-container">
+              <div className="header-content">
+                <p className="info-needed">이름</p>
+                <input type="text" className="info-received" />
+              </div>
+              <div className="header-content">
+                <p className="info-needed">휴대폰 번호</p>
+                <input type="text" className="info-received" />
+              </div>
+              <div className="header-content">
+                <p className="info-needed">생년월일</p>
+                <input
+                  type="date"
+                  id="start"
+                  min="1900.01.01"
+                  max="2015.12.31"
+                  className="info-received"
+                />
+              </div>
+            </div>
+          </div>
+          <form className="content">
+            <div className="inputs">
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="아이디 (영문 또는 숫자 4-12자)"
+              />
+              <input
+                type="password"
+                id="pw"
+                name="pw"
+                placeholder="비밀번호 입력(영문,숫자,특수문자 조합 8-16자)"
+              />
+              <input
+                type="password"
+                id="pw"
+                name="pw"
+                placeholder="비밀번호 확인"
+              />
+            </div>
+            <div className="button-area">
+              <button disabled>회원가입</button>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
