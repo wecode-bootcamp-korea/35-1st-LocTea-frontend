2b9e300640749bbeab87262aabc3284a56b62f61
김익현 <kimikhyun@ikkim01-MacBookAir.local>
2022-07-19 15:08:18 +0900

    Add:메인페이지 공지사항

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Notice from './Notice/Notice';
 import './Main.scss';
+
 const Main = () => {
-  return <div />;
+  return (
+    <div>
+      <Notice />
+    </div>
+  );
 };
 
 export default Main;
