40a1b221e246d61697b594fe07f882ddaf15f33c
이금관 <igeumgwan@eric.local>
2022-07-19 09:26:15 +0900

    ad: 로그인 화면 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,56 @@
 import React from 'react';
 import './Login.scss';
 const Login = () => {
-  return <div />;
+  return (
+    <div>
+      <header>
+        <div className="login-nav">
+          <h1>로그인</h1>
+          <button type="submit">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+      </header>
+      <section className="body">
+        <div className="login-container">
+          <div className="header">
+            <p>아모레퍼시픽 뷰티포인트 통합회원</p>
+            <p>아이디로 로그인해주세요.</p>
+          </div>
+          <form className="content">
+            <div className="inputs">
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="아이디 입력"
+              ></input>
+              <input
+                type="password"
+                id="pw"
+                name="pw"
+                placeholder="비밀번호 입력(영문,숫자,특수문자 조합)"
+              ></input>
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" id="checkbox" />
+              아이디 저장
+            </div>
+            <div className="button-area">
+              <button disabled>로그인</button>
+            </div>
+          </form>
+          <div className="footer-border">
+            <div className="footer">
+              <i class="fa-solid fa-mobile-screen"></i>
+              <img src="../images/login/Kakao.png" />
+              <img src="../images/login/naver.png" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
