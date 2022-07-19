b3433ef8d61d29d97a43a0f6a5ffc8fba70d0a01
이금관 <igeumgwan@eric.local>
2022-07-19 10:54:13 +0900

    register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './Register.scss';
 const Register = () => {
-  return <div />;
+  return (
+    <div>
+      {' '}
+      <section className="register-header">
+        <div className="register-nav">
+          <h1>회원 가입</h1>
+          <button type="submit">
+            <i class="fa-solid fa-xmark" />
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Register;
