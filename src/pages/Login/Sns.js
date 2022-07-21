c9d63ee0a9c0a3df4049094f6d384beb71e07ef3
이금관 <igeumgwan@eric.local>
2022-07-21 11:29:06 +0900

    sider 수정 완

--- src/pages/Login/Sns.js
+++ src/pages/Login/Sns.js
@@ -3,7 +3,7 @@ import SNS_LIST from './SnsData';
 export default function Sns() {
   return SNS_LIST.map(info => {
     return (
-      <div className="login-footer-text">
+      <div key={info.id} className="login-footer-text">
         <img className="phone" src={info.img} alt="" />
         <p>{info.text}</p> <p>{info.text2}</p>
       </div>
