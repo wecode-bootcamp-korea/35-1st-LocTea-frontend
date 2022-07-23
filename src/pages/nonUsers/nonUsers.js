2d83c58c97dccc366c46968bb2224b9ed22e38df
김익현 <kimikhyun@ikkim01-MacBookAir.local>
2022-07-23 21:53:13 +0900

    Add:주문고객정보까지 레이아웃 구성완료

--- src/pages/nonUsers/nonUsers.js
+++ src/pages/nonUsers/nonUsers.js
@@ -3,15 +3,15 @@ import './nonUsers.scss';
 
 const NonUsers = () => {
   return (
-    <div className="nonUsers">
-      <div className="nonUsers-title">
-        <h1>결제하기</h1>
+    <div className="nonusers">
+      <div className="nonusers-title">
+        <h2>결제하기</h2>
       </div>
-      <form name="form-order">
+      <form name="form-order" className="form-order">
         <div className="form-order-left">
           <div className="order-user-info">
             <div className="order-user-title">
-              <h6>주문고객정보</h6>
+              <p>주문고객정보</p>
             </div>
             <div className="order-user-name">
               <p>이름</p>
@@ -19,17 +19,44 @@ const NonUsers = () => {
             </div>
             <div className="order-user-phone">
               <p>휴대전화</p>
-              <input type="number" />
+              <div className="mobile-number-box">
+                <input type="hidden" id="user-phone" name="customerMobileNo" />
+                <div className="inp-selectbox">
+                  <select id="user-phone1" className="inp-select">
+                    <option value="010" selected="">
+                      010
+                    </option>
+                    <option value="011">011</option>
+                    <option value="016">016</option>
+                    <option value="017">017</option>
+                    <option value="018">018</option>
+                    <option value="019">019</option>
+                  </select>
+                </div>
+                <span className="hyphen">-</span>
+                <input
+                  type="text"
+                  id="user-phone2"
+                  className="inp-text mobile-number"
+                  title="휴대전화 입력"
+                  value=""
+                  placeholder="'-' 없이 휴대폰번호 입력"
+                  max="8"
+                  maxlength="8"
+                  inputmode="numeric"
+                  pattern="[0-9]*"
+                />
+              </div>
             </div>
             <div className="order-user-sendingname">
               <p>보내는 분</p>
               <div className="sendingname-input">
                 <input placeholder="보내는 분 입력(7자이내)" type="text" />
-                <ul>
+                <ul className="textbox">
                   <li>
                     보내는 분 별도 표기하더라도 고객님 정보보호를 위해 마스킹
                     처리되어 발송됩니다.
-                    <span>예) 위코드 -> 위*드 / 최강록차 -> 최강**</span>
+                    <span>예) 위코드 -&gt; 위*드 / 최강록차 -&gt; 최강**</span>
                   </li>
                 </ul>
               </div>
@@ -66,7 +93,7 @@ const NonUsers = () => {
             </div>
           </div>
         </div>
-        <div className="form-order-right" />
+        <div className="form-order-right">a</div>
       </form>
     </div>
   );
