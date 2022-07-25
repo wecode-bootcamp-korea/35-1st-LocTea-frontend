f77bcb71ec14d551abbf22240828bb513248fbe7
이금관 <igeumgwan@eric.local>
2022-07-25 20:58:07 +0900

    fix

---
+++ src/pages/Main/BtnSlider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BtnSlider = () => {
+  return (
+    <div>
+      <button className="btn-slide">
+        <i class="fa-solid fa-angle-left" />
+      </button>
+    </div>
+  );
+};
+
+export default BtnSlider;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HeaderImg from './Header';
+import BtnSlider from './BtnSlider';
 import './Main.scss';
 const Main = () => {
   return (
     <div className="main-header">
       <div className="main-header-container">
         <HeaderImg />
+        <BtnSlider />
+        <BtnSlider />
       </div>
     </div>
   );
