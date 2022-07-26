977f035eb28d8809cce6ea59db06dfe3706ada60
이금관 <igeumgwan@eric.local>
2022-07-26 21:15:50 +0900

    add: 제품 navigate 추가

--- src/Components/Nav/ItemType.js
+++ src/Components/Nav/ItemType.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ItemType({ values }) {
+  const navigate = useNavigate();
+
   return values.map(info => {
     return (
       <div key={info.first_category_id} className="category-box">
-        <button className="item-title-button">{info.title}</button>
+        <button
+          className="item-title-button"
+          onClick={() =>
+            navigate(`/products/list?first-category=${info.first_category_id}`)
+          }
+        >
+          {info.title}
+        </button>
         <button>
           <div className="item-lists-container">
             {info.second_categories.map(input => {
               return (
                 <li key={input.second_category_id}>
-                  <button className="item">{input.title}</button>
+                  <button
+                    className="item"
+                    onClick={() =>
+                      navigate(
+                        `/products/list?second-category=${input.second_category_id}`
+                      )
+                    }
+                  >
+                    {input.title}
+                  </button>
                 </li>
               );
             })}
