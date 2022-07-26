98252ece759582e75a8295b2792f9f21ded46b51
이금관 <igeumgwan@eric.local>
2022-07-26 20:50:19 +0900

    a

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-
 import Main from './pages/Main/Main';
 import Nav from './Components/Nav/Nav';
 import Cart from './pages/Cart/Cart';
