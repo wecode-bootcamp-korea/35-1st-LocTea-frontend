efb71e00a9b848d9274ed3a40a3d7d425c406def
최재홍 <choejaehong@choejaehong-ui-MacBookPro.local>
2022-07-28 15:26:19 +0900

    -d

--- src/pages/ItemList/Components/Order.js
+++ src/pages/ItemList/Components/Order.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './Order.scss';
 
 const Order = ({ whichProductRender, whatOrder, setWhatOrder }) => {
