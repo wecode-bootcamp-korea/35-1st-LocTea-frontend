797118965fd278bda34fb04abe23c01ccb795c0e
이금관 <igeumgwan@eric.local>
2022-07-28 16:29:35 +0900

    주소 변경

--- src/Components/Nav/ItemType.js
+++ src/Components/Nav/ItemType.js
@@ -3,38 +3,35 @@ import { useNavigate } from 'react-router-dom';
 
 export default function ItemType({ values }) {
   const navigate = useNavigate();
-
   return values.map(info => {
     return (
       <div key={info.first_category_id} className="category-box">
         <button
           className="item-title-button"
           onClick={() =>
-            navigate(`/products/list?first-category=${info.first_category_id}`)
+            navigate(`/itemlist/first-category/${info.first_category_id}`)
           }
         >
           {info.title}
         </button>
-        <button>
-          <div className="item-lists-container">
-            {info.second_categories.map(input => {
-              return (
-                <li key={input.second_category_id}>
-                  <button
-                    className="item"
-                    onClick={() =>
-                      navigate(
-                        `/products/list?second-category=${input.second_category_id}`
-                      )
-                    }
-                  >
-                    {input.title}
-                  </button>
-                </li>
-              );
-            })}
-          </div>
-        </button>
+        <div className="item-lists-container">
+          {info.second_categories.map(input => {
+            return (
+              <li key={input.second_category_id}>
+                <button
+                  className="item"
+                  onClick={() =>
+                    navigate(
+                      `/itemlist/second-category/${input.second_category_id}`
+                    )
+                  }
+                >
+                  {input.title}
+                </button>
+              </li>
+            );
+          })}
+        </div>
       </div>
     );
   });
